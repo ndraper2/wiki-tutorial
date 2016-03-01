8dac3dc8cf9632e26d6ac833cc9350a71810d1a7
Nicholas Draper <ndraper2@gmail.com>
2016-03-01 13:19:19 -0800

    add wiki app to installed apps, create model Page

--- wikitutorial/wiki/models.py
+++ wikitutorial/wiki/models.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    content = models.TextField(blank=True)
